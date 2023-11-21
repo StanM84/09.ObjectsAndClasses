6ff80a343d9689599a84dc9bca4efbb9ab062061
StanM84 <stan1984@abv.bg>
2023-11-21 15:34:06 +0200

    Add project files.

---
+++ 03.StoreBoxes/03.StoreBoxes.cs
@@ -0,0 +1,62 @@
+﻿namespace _03.StoreBoxes
+{
+    public class Item
+    {
+        public Item(string name, decimal price)
+        {
+            Name = name;
+            Price = price;
+        }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public class Box
+    {
+        public Box(string serialNumber, Item item, int itemQuantity)
+        {
+            SerialNumber = serialNumber;
+            Item = item;
+            ItemQuatity = itemQuantity;
+            PriceOfABox = item.Price * itemQuantity;
+        }
+        public string SerialNumber { get; set; }
+        public Item Item { get; set; }
+        public int ItemQuatity { get; set; }
+        public decimal PriceOfABox { get; set; }
+
+
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            List<Box> boxes = new List<Box>();
+
+            while (command != "end") 
+            {
+                string[] data = command.Split(" ");
+                string serialNumber = data[0];
+                string itemName = data[1];
+                int itemQantity = int.Parse(data[2]);
+                decimal itemPrice = decimal.Parse(data[3]);
+
+                Item currentItem = new Item(itemName, itemPrice);
+                Box currentBox = new Box(serialNumber, currentItem, itemQantity);
+                boxes.Add(currentBox);
+
+                command = Console.ReadLine();
+            }
+            boxes = boxes.OrderByDescending(x => x.PriceOfABox).ToList();
+
+            foreach (Box box in boxes)
+            {
+                Console.WriteLine($"{box.SerialNumber}");
+                Console.WriteLine($"-- {box.Item.Name} - ${box.Item.Price:F2}: {box.ItemQuatity}");
+                Console.WriteLine($"-- ${box.PriceOfABox:F2}");
+            }
+        }
+    }
+}
---
+++ 1.Songs/1.Songs.cs
@@ -0,0 +1,60 @@
+﻿namespace _1.Songs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfSongs = int.Parse(Console.ReadLine());
+            List<Song> songs = new List<Song>();
+
+            for (int i = 0; i < numberOfSongs; i++)
+            {
+                string[] songsComponent = Console.ReadLine().Split("_");
+                string songTypeList = songsComponent[0];
+                string songName = songsComponent[1];
+                string songTime = songsComponent[2];
+
+                Song curentSong = new Song { Name = songName, TypeList = songTypeList, Time = songTime};
+                
+                songs.Add(curentSong);
+            }
+
+            string typeComand = Console.ReadLine();
+
+            if (typeComand != "all")
+            {
+                songs = songs.Where(x => x.TypeList == typeComand).ToList();
+            }
+            foreach (Song song in songs) 
+            {
+                Console.WriteLine(song.Name);
+            }
+
+            //if (typeComand == "all")
+            //{
+            //    foreach (Song song in songs) 
+            //    {
+            //        Console.WriteLine(song.Name);
+            //    }
+            //}
+            //else
+            //{
+            //    List<Song> filteredSongs = songs.Where(x => x.TypeList == typeComand).ToList();
+
+            //    foreach (Song song in filteredSongs)
+            //    {
+            //        Console.WriteLine(song.Name);
+            //    }
+            //}
+        }
+    }
+
+    public class Song
+    {
+        public string Name { get; set; }
+
+        public string Time { get; set; }
+
+        public string TypeList { get; set; }
+    }
+}
